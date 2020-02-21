3398c12821a0514342a92934c7d811b57f783e6c
skroderi <bakaminski@wp.pl>
2020-02-21 16:11:00 +0100

    component communication,usage

---
+++ Component usage/src/components/Quote.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"> </slot>
+      <span style="color: #ccc"><slot name="subtitle">The subtitle</slot></span>
+    </div>
+    <hr />
+    <div>
+      <slot> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 0 auto;
+}
+h2 {
+  color: red;
+}
+
+.title {
+  font-style: italic;
+}
+</style>
