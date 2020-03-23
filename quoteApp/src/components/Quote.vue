fabe6473cac5d209bb525498c80324d83ec90760
Skroderi <Bakaminski@wp.pl>
2020-03-24 00:36:21 +0100

    add quotes

---
+++ quoteApp/src/components/Quote.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="col-sm-6 col-md-4 cd-lg-3">
+    <div class="panel panel-default">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"]
+};
+</script>
+
+<style>
+.panel-body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+</style>
