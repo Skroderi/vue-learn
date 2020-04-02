b710248111761135b9b5336569a8423ac7858648
Skroderi <Bakaminski@wp.pl>
2020-04-02 21:11:36 +0200

    mixins and filters

--- Local filter/src/List.vue
+++ Local filter/src/List.vue
@@ -3,37 +3,24 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{ text | toUpercase | tolowercase }}</p>
         <hr />
         <input v-model="filterText" />
         <ul>
           <li v-for="fruit in filteredFruits">{{fruit}}</li>
         </ul>
         <hr />
-        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import List from "./List.vue";
 import { fruitMixin } from "./fruitMixin";
 
 export default {
   mixins: [fruitMixin],
-  data() {
-    return {
-      text: "hello there"
-    };
-  },
-  filters: {
-    toUppercase(value) {
-      return value.toUpercase();
-    }
-  },
-  components: {
-    appList: List
+  created() {
+    console.log("created in list component");
   }
 };
 </script>
